85e25afd19971e89ac9d05fe38b5c2793017cf8b
Ad <adrienroux94@gmail.com>
2021-02-21 20:52:28 +0100

    Test d'initialisation d'un monde et récupération des factions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,23 @@
+import World.World;
+import org.json.JSONObject;
+
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        jsonParser json = new jsonParser();
-        json.displayJson();
+        jsonParser json = new jsonParser("Json/attackOnTitans.json");
+
+        World world = json.initWorldFromJsonData("Monde");
+
+        Arrays.asList(world.getIsland().getFactions()).forEach(System.out::println);
     }
 }
