e4aeb6240f397a8d66949ba11e36416affe5ea22
Ad <adrienroux94@gmail.com>
2021-02-21 21:59:38 +0100

    Modification des classes d'effets

--- src/main/java/History/FactionsEffects.java
+++ src/main/java/History/FactionsEffects.java
@@ -1,4 +1,12 @@
 package History;
 
 public class FactionsEffects {
+
+    private String name;
+    private int effect;
+
+    public FactionsEffects(String name, int effect){
+        this.name = name;
+        this.effect = effect;
+    }
 }
--- src/main/java/History/ResourcesEffects.java
+++ src/main/java/History/ResourcesEffects.java
@@ -1,4 +1,12 @@
 package History;
 
 public class ResourcesEffects {
+
+    private String name;
+    private int effect;
+
+    public ResourcesEffects(String name, int effect){
+        this.name = name;
+        this.effect = effect;
+    }
 }
