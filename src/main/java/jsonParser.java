8c8c342550633c6b04d5234aadd0e60f92ed119b
Ad <adrienroux94@gmail.com>
2021-02-21 20:52:03 +0100

    Traitement de JSON donné par fichier et création du monde

--- src/main/java/jsonParser.java
+++ src/main/java/jsonParser.java
@@ -1,4 +1,12 @@
 import Factions.Factions;
+import History.EventChoices;
+import History.Events;
+import History.Scenario;
+import Resources.Agriculture;
+import Resources.Industry;
+import Resources.Treasury;
+import World.World;
+import com.sun.javafx.scene.control.behavior.TwoLevelFocusBehavior;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -14,14 +22,31 @@ import java.util.stream.Collectors;
 
 public class jsonParser {
 
+    JSONObject json;
+
+    jsonParser(String fileName) {
+        this.json = new JSONObject(getContentStringFromResource(fileName));
+    }
+
+    public World initWorldFromJsonData(String name){
+        World world = new World();
+        world.initIsland(
+                name,
+                initFactionsFromFile(),
+                initAgriculture(),
+                initIndustry(),
+                initTreasury(),
+                initScenario()
+
+        );
+        return world;
+    }
+
     public void displayJson() {
-        String json = getContentStringFromResource("Json/attackOnTitans.json");
 
-        JSONObject obj = new JSONObject(json);
-
-        JSONArray key = obj.names();
+        JSONArray key = json.names();
         System.out.println(key);
-        JSONArray keyb = obj.getJSONArray("events");
+        JSONArray keyb = json.getJSONArray("events");
         System.out.println(keyb.length());
     }
 
@@ -32,12 +57,10 @@ public class jsonParser {
                 .collect(Collectors.joining("\n"));
     }
 
-     public Factions[] initFactionsFromFile(String file) {
-         String json = getContentStringFromResource(file);
-         JSONObject obj = new JSONObject(json);
+     private Factions[] initFactionsFromFile() {
 
-         JSONObject factionsData = obj.getJSONObject("gameStartParameters").getJSONObject("NORMAL").getJSONObject("factions");
-         List<Factions> factionsList = new ArrayList<Factions>();
+         JSONObject factionsData = json.getJSONObject("gameStartParameters").getJSONObject("NORMAL").getJSONObject("factions");
+         List<Factions> factionsList = new ArrayList<>();
 
          factionsData.keySet().forEach(factionName -> {
              String name = null;
@@ -65,4 +88,72 @@ public class jsonParser {
 
          return factionsList.toArray(new Factions[0]);
      }
+
+     private Agriculture initAgriculture(){
+        return new Agriculture(Integer.parseInt(json.getJSONObject("gameStartParameters").getJSONObject("NORMAL").get("agriculturePercentage").toString()));
+     }
+
+    private Industry initIndustry(){
+        return new Industry(Integer.parseInt(json.getJSONObject("gameStartParameters").getJSONObject("NORMAL").get("industryPercentage").toString()));
+    }
+
+    private Treasury initTreasury(){
+        return new Treasury(Integer.parseInt(json.getJSONObject("gameStartParameters").getJSONObject("NORMAL").get("treasury").toString()));
+    }
+
+    private Scenario initScenario(){
+        return new Scenario(
+                json.get("name").toString(),
+                json.get("story").toString(),
+                initEvents()
+        );
+    }
+
+    private ArrayList<Events> initEvents(){
+        ArrayList<Events> events = new ArrayList<>();
+
+        JSONArray eventsData = json.getJSONArray("events");
+
+        for (int x = 0; x < eventsData.length(); x++) {
+            String name = null;
+            JSONObject eventProperties = (JSONObject) eventsData.get(x);
+
+            for (int i = 0; i < eventProperties.keySet().toArray().length; i++){
+
+                Object key = eventProperties.keySet().toArray()[i];
+                Object value = eventProperties.get(key.toString());
+
+                if (key.equals("name")) {
+                    name = value.toString();
+                }
+            }
+
+            events.add(new Events(name, initChoices(eventProperties.getJSONArray("choices"))));
+        }
+
+        return events;
+    }
+
+    private ArrayList<EventChoices> initChoices(JSONArray choices){
+        ArrayList<EventChoices> eventChoices = new ArrayList<>();
+
+        for (int x = 0; x < choices.length(); x++) {
+            String name = null;
+
+            JSONObject choiceProperties = (JSONObject) choices.get(x);
+
+            for (int i = 0; i < choiceProperties.keySet().toArray().length; i++){
+
+                Object key = choiceProperties.keySet().toArray()[i];
+                Object value = choiceProperties.get(key.toString());
+
+                if (key.equals("choice")) {
+                    name = value.toString();
+                }
+            }
+
+            eventChoices.add(new EventChoices(name, new ArrayList<>(), new ArrayList<>()));
+        }
+        return eventChoices;
+    }
 }
