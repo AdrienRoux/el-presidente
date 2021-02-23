17deb57ea895f6912b53be38b075d9b1ec918df9
Ad <adrienroux94@gmail.com>
2021-02-23 18:39:00 +0100

    Modification du main pour l'affichage du début de partie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,16 @@
 import World.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import org.json.JSONObject;
-
-import javax.swing.*;
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws URISyntaxException {
+        World.displayGameStart();
+
         jsonParser json = new jsonParser(jsonParser.getFilePath());
 
         World world = json.initWorldFromJsonData("Monde");
+
+        world.initGame();
 
         //System.out.println("Difficulty chosen : " + world.getDifficulty());
 
