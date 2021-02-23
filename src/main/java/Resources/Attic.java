236a83b1ec6cdd489a7def151086967bbb33887b
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2021-02-23 17:10:49 +0100

    Update Attic.java
    update 23/02  mehdi ennasiri

--- src/main/java/Resources/Attic.java
+++ src/main/java/Resources/Attic.java
@@ -1,5 +1,7 @@
 package Resources;
 
+import Factions.Factions;
+
 import java.util.ArrayList;
 
 public class Attic {
@@ -17,6 +19,46 @@ public class Attic {
         this.foodStocked.add(new FoodBatch(quantity,0 ));
     }
 
-    public void feedPopulation(){}
+    public void feedPopulation(Factions[] factions)
+    {
+        int globalPopulation = 0;
+        for(int i = 0; i < factions.length; i++)
+        {
+            globalPopulation = globalPopulation + factions[i].getPopulation();
+        }
+        int neededFood = globalPopulation*4;
+        int globalFood = getAllFoodStocked();
+
+        if(neededFood < globalFood)
+        {
+
+        }
+        else
+        {
+            // display pas assez de bouf go acheter
+        }
+    }
+
+    public void consumeFood(int neededFood)
+    {
+        int i = 0;
+        while (neededFood > 0 )
+        {
+            int stock = this.foodStocked.get(i).getFoodQuantity();
+            
+        }
+    }
+
+    public int getAllFoodStocked()
+    {
+        int globalFood = 0;
+
+        for (int i = 0; i<this.foodStocked.size(); i++)
+        {
+            globalFood = globalFood + foodStocked.get(i).getFoodQuantity();
+        }
+
+        return globalFood;
+    }
 
 }
