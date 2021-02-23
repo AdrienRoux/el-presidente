2b0f172f3b61fc7b475bd89e35ec4687ceb525d2
Ad <adrienroux94@gmail.com>
2021-02-23 18:38:21 +0100

    Début de la mise en forme de l'affichage du jeu

--- src/main/java/World/World.java
+++ src/main/java/World/World.java
@@ -6,6 +6,8 @@ import Resources.Agriculture;
 import Resources.Industry;
 import Resources.Treasury;
 
+import org.apache.commons.lang3.text.WordUtils;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -29,15 +31,27 @@ public class World {
 
     private void setDifficulty(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choisissez la difficulté :");
+        System.out.println("Choisissez la difficulté :\n");
         for (int i = 0; i < Difficulty.values().length; i++) {
             System.out.println(i + ": " + Arrays.stream(Difficulty.values()).toArray()[i]);
         }
+        System.out.println();
         int choice = Integer.parseInt(sc.nextLine());
         difficulty = Difficulty.valueOf(Arrays.stream(Difficulty.values()).toArray()[choice].toString());
     }
 
     public Difficulty getDifficulty() {
         return difficulty;
+    }
+
+    public static void displayGameStart(){
+        System.out.println("Welcome to El Presidente !");
+        System.out.println("\n----------------------------------\n");
+    }
+
+    public void initGame(){
+        String story = this.getIsland().getScenario().getStory();
+        String name = this.getIsland().getScenario().getName();
+        System.out.println("\nHistoire : " + name + "\n" + WordUtils.wrap(story, 100));
     }
 }
