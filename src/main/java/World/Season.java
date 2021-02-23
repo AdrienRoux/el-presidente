53db4ef1fab8f302c5ed8d6a7b9d34a7bccf7689
Ad <adrienroux94@gmail.com>
2021-02-23 18:51:17 +0100

    Ajout des saisons dans l'enum

--- src/main/java/World/Season.java
+++ src/main/java/World/Season.java
@@ -1,4 +1,23 @@
 package World;
 
 public enum Season {
+    SPRING ("Printemps"),
+    SUMMER ("Ete"),
+    AUTUMN ("Automne"),
+    WINTER ("Hiver");
+
+    private final String name;
+
+    Season(String s) {
+        name = s;
+    }
+
+    public boolean equalsName(String otherName) {
+        // (otherName == null) check is not needed because name.equals(null) returns false
+        return name.equals(otherName);
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
