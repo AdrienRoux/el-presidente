a6944a5330595033167736c1813b69ed7a158431
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2021-03-06 17:15:18 +0100

    add test directories

---
+++ src/test/java/WorldTest/TestIsland.java
@@ -0,0 +1,20 @@
+package WorldTest;
+
+import World.Island;
+import junit.framework.TestCase;
+
+
+public class TestIsland extends TestCase{
+
+
+    public void setUp()
+    {
+        Island island = new Island();
+    }
+
+
+    public void test()
+    {
+
+    }
+}
