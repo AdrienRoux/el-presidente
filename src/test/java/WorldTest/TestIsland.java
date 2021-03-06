37af49226e5dbaf346145c7fe861f30a9f0675aa
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2021-03-06 17:32:04 +0100

    update setUp testIsland

--- src/test/java/WorldTest/TestIsland.java
+++ src/test/java/WorldTest/TestIsland.java
@@ -1,7 +1,15 @@
 package WorldTest;
 
+import Factions.Factions;
+import History.Scenario;
+import Resources.Agriculture;
+import Resources.Attic;
+import Resources.Industry;
+import Resources.Treasury;
 import World.Island;
 import junit.framework.TestCase;
+
+import java.lang.reflect.Array;
 
 
 public class TestIsland extends TestCase{
@@ -9,7 +17,20 @@ public class TestIsland extends TestCase{
 
     public void setUp()
     {
-        Island island = new Island();
+        Treasury treasury = new Treasury(500);
+        Agriculture agriculture = new Agriculture(50);
+        Industry industry = new Industry(50);
+        Attic attic = new Attic(500);
+        Scenario scenario = new Scenario("akha","akkhhhaaaaaa", null);
+        Factions[] factions = new Factions[3];
+        Factions faction1 = new Factions("facion 1", 60,50);
+        Factions faction2 = new Factions("facion 2", 65,50);
+        Factions faction3 = new Factions("facion 3", 70,50);
+        factions[0] = faction1;
+        factions[1] = faction2;
+        factions[2] = faction3;
+
+        Island island = new Island("ile",factions,agriculture,industry,treasury,scenario,attic);
     }
 
 
