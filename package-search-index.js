28ace71839a32eff7580fe679008a7cf532f51bb
Ad <adrienroux94@gmail.com>
2021-03-08 00:10:06 +0100

    update javadoc

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Factions"},{"l":"History"},{"l":"Resources"},{"l":"ResourcesTest"},{"l":"World"},{"l":"WorldTest"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Factions"},{"l":"History"},{"l":"JsonParser"},{"l":"Resources"},{"l":"ResourcesTest"},{"l":"World"},{"l":"WorldTest"}]
