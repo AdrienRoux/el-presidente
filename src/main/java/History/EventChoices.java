2a9acc6aa85e7564bdd3ce8d1494e14de80108ef
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2021-03-07 22:28:04 +0100

    event choices update

--- src/main/java/History/EventChoices.java
+++ src/main/java/History/EventChoices.java
@@ -1,5 +1,7 @@
 package History;
 
+import World.Island;
+
 import java.util.ArrayList;
 
 public class EventChoices {
@@ -7,11 +9,23 @@ public class EventChoices {
     private String name;
     private ArrayList<FactionsEffects> factionEffects;
     private ArrayList<ResourcesEffects> resourcesEffects;
+    private Events relatedEvent; 
 
     public EventChoices(String name, ArrayList<FactionsEffects> factionEffects, ArrayList<ResourcesEffects> resourcesEffects){
         this.name = name;
         this.factionEffects = factionEffects;
         this.resourcesEffects = resourcesEffects;
+        
+    }
+    
+    public void launchEffect(Island island)
+    {
+        factionEffects.forEach( effect ->
+                effect.launchEffect(island)
+        );
+        resourcesEffects.forEach( effect ->
+                effect.launchEffect(island)
+        );
     }
 
 
